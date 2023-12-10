6a8e3adab4e5af5f5337f2a75e801a6b8d3d5185
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-10 13:58:10 -0300

    [ :sparkles: ] | implements: service - email (application)

--- src/main/java/com/ms/email/application/service/EmailServiceImpl.java
+++ src/main/java/com/ms/email/application/service/EmailServiceImpl.java
@@ -5,11 +5,15 @@ import com.ms.email.application.usecase.email.SendEmailUseCase;
 import com.ms.email.application.usecase.email.SetupEmailUseCase;
 import com.ms.email.domain.entity.Email;
 import static com.ms.email.domain.entity.EmailStatus.*;
+
+import com.ms.email.domain.policy.email.UpdateEmailStatusOnSendPolicy;
 import com.ms.email.domain.service.EmailService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -18,14 +22,15 @@ public class EmailServiceImpl implements EmailService {
     @Autowired private SendEmailUseCase send;
     @Autowired private SaveEmailUseCase save;
 
+    @Autowired private UpdateEmailStatusOnSendPolicy updateEmailStatusOnSendPolicy;
+
 
     @Override @Transactional
-    public Email sendEmail(Email email) {
-        email = setup.execute(email);
+    public Optional<Email> sendEmail(Email email) {
 
-        try{ send.execute(email); }
-        catch (MailException e) { email.setStatus(ERROR); }
-
-        return save.execute(email);
+        email.setStatus(ERROR);
+        return updateEmailStatusOnSendPolicy
+                .execute(() -> send.execute(setup.execute(email)))
+                .map(save::execute);
     }
 }
