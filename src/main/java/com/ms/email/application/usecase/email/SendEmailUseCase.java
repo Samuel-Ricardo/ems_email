2a33d7222ab4a8ca401119465f951b9ed9f546e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-10 10:14:25 -0300

    [ :sparkles: ] | create: use-case - send > email (application)

---
+++ src/main/java/com/ms/email/application/usecase/email/SendEmailUseCase.java
@@ -0,0 +1,23 @@
+package com.ms.email.application.usecase.email;
+
+import com.ms.email.domain.entity.Email;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendEmailUseCase {
+
+    @Autowired private JavaMailSender mailSender;
+
+    public void execute(Email email) {
+        var message = new SimpleMailMessage();
+
+        message.setTo(email.getEmailTo());
+        message.setSubject(email.getSubject());
+        message.setText(email.getTextBody());
+
+        mailSender.send(message);
+    }
+}
