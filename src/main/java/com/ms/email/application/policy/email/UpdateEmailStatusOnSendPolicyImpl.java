9ddd630f20a96ff3f1b2ec749533381f84c2e96a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-10 14:00:30 -0300

    [ :scroll: ] | implements: policy - update > email [status] (application)

--- src/main/java/com/ms/email/application/policy/email/UpdateEmailStatusOnSendPolicyImpl.java
+++ src/main/java/com/ms/email/application/policy/email/UpdateEmailStatusOnSendPolicyImpl.java
@@ -1,16 +1,29 @@
 package com.ms.email.application.policy.email;
 
 import com.ms.email.domain.entity.Email;
+import com.ms.email.domain.entity.EmailStatus;
 import com.ms.email.domain.policy.email.UpdateEmailStatusOnSendPolicy;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
 @Component
 public class UpdateEmailStatusOnSendPolicyImpl implements UpdateEmailStatusOnSendPolicy {
 
     @Override
-    public Email execute(Callable<Email> send) {
-        return null;
+    public Optional<Email> execute(Callable<Email> send) {
+
+        Optional<Email> email = Optional.empty();
+
+        try {
+            email = Optional.of(send.call());
+            email.ifPresent(it -> it.setStatus(EmailStatus.SENT));
+        }
+
+        catch (Exception e) { e.printStackTrace();}
+
+        return email;
     }
 }
